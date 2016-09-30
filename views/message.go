370b87c14fe29d7d7e2e06c455181c96450f0734
Depado <paul.lhussiez@gmail.com>
2016-09-30 23:02:01 +0200

    Well I guess it works

--- views/message.go
+++ views/message.go
@@ -1,7 +1,9 @@
 package views
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,6 +22,22 @@ func PostMessage(c *gin.Context) {
 	var form SendForm
 	if err = c.Bind(&form); err != nil {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{"athletes": athletes, "posted": "errors"})
+		return
+	}
+	var resp *http.Response
+	v := url.Values{}
+	v.Add("epr", "187")
+	v.Add("bib", form.Athlete)
+	v.Add("nam", form.Name)
+	v.Add("mel", form.Mail)
+	v.Add("msg", form.Message)
+	if resp, err = http.Get(fmt.Sprintf("http://www.marchons.com/directlive/doencourage.php?%v", v.Encode())); err != nil {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{"athletes": athletes, "posted": "remote_error"})
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{"athletes": athletes, "posted": "remote_error"})
 		return
 	}
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{"athletes": athletes, "posted": "success"})
